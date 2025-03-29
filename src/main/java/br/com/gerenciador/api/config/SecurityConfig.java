85cc81b1e1e8503b115e6db48e01f8a1c71b480c
kelsonalmeida <kelson.victr@gmail.com>
2025-03-29 09:34:52 -0300

    fix: cors vercel

--- src/main/java/br/com/gerenciador/api/config/SecurityConfig.java
+++ src/main/java/br/com/gerenciador/api/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://gp-prof-front-mqoc.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
